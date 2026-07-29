95626807240f164360ce7f7d89c74781752a70e7
Sargius <Dr.Sargius@gmail.com>
2026-07-29 20:16:30 +0400

    - chore: updated README and version for merging to master.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,9 @@ android {
         minSdk = 24
         targetSdk = 37
         versionCode = 1
-        versionName = "1.0.0"
+        versionName = "1.0.0 merge 2"
 
+        // vc 1.0.0 merge 2 - ANARCH-11 Implement Dashboard Orchestration and Navigation
         // vc 1.0.0 - initial complete permission's livecycle
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
